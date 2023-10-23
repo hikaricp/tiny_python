089525d652e37debdaacef82cc44486fc60e126b
hikaricp <dockerc@163.com>
2023-10-24 01:11:32 +0800

    编写并测试程序

---
+++ 01_hello/test_hello.py
@@ -0,0 +1,20 @@
+"""
+Author: Jiachang Yang <dockerc@163.com>
+Summary: 测试程序
+"""
+
+
+def test_exists():
+    print("文件存在")
+
+
+def test_runnable():
+    print("Hello, world!")
+
+
+def test_usage():
+    print("使用说明")
+
+
+def test_input():
+    print("输入参数")
